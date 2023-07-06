de49f85049136a9e8b5d9a2c7696303d09483b51
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-07-06 21:01:14 +0530

    login validation for front end

--- src/components/forms/Basicform.js
+++ src/components/forms/Basicform.js
@@ -4,9 +4,17 @@ import './Basicform.css';
 const Basicform = () => {
     const[email,setEmail] = useState("");
     const[password,setPassword] = useState("");
+    const[allEntry,setAllEntry] = useState([]);
+
+    const submitform = (e)=>{
+        e.preventDefault();
+
+        const newEntry = {email: email, password: password};
+        setAllEntry([...allEntry,newEntry]);
+    }
   return (
     <>
-      <form className="Basicform">
+      <form className="Basicform" onSubmit={submitform}>
       <div>
           <label className="email">email</label>
           <input className="email-input" type="email"
@@ -24,6 +32,19 @@ const Basicform = () => {
         </div>
         <button type="submit">Login</button>
       </form>
+      <div>
+        {
+            allEntry.map((curElem)=>{
+              return (
+                <div>
+                    <p>{curElem.email}</p>
+                    <p>{curElem.password}</p>
+                    
+                </div>
+              )  
+            })
+        }
+      </div>
     </>
   );
 };
