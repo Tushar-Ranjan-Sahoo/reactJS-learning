298304dd1ee02d9fb10cf8198dc6b06960b606c2
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2023-07-06 20:42:43 +0530

    on change

--- src/components/forms/Basicform.js
+++ src/components/forms/Basicform.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import './Basicform.css';
 
 const Basicform = () => {
+    const[email,setEmail] = useState("");
+    const[password,setPassword] = useState("");
   return (
     <>
       <form className="Basicform">
       <div>
           <label className="email">email</label>
-          <input className="email-input" type="email" />
+          <input className="email-input" type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label>password</label>
-          <input type="password" />
+          <input type="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          
         </div>
         <button type="submit">Login</button>
       </form>
